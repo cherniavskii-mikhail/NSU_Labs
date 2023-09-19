660a0a7fe6243a3b03b5d630845b8e85727b3c48
3uPaeJLb <131700005+3uPaeJLb@users.noreply.github.com>
2023-09-19 21:51:33 +0700

    Create lab1.2.c

---
+++ C/lab1.2/lab1.2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() 
+{
+    int num1, num2;
+
+    scanf_s("%d", &num1);
+
+    scanf_s("%d", &num2);
+
+    printf("num1 = %d, num2 = %d\n", num1, num2);
+
+  //  swap(&num1, &num2);
+    num1 = num1 + num2;
+    num2 = num1 - num2;
+    num1 = num1 - num2;
+
+    printf("num1 = %d, num2 = %d\n", num1, num2);
+
+    return 0;
+}
