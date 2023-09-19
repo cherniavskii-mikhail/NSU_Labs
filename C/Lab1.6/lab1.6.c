ad4e8e496d81a7fdbd8084768b269da95683c868
3uPaeJLb <131700005+3uPaeJLb@users.noreply.github.com>
2023-09-19 21:57:36 +0700

    Create lab1.6.c

---
+++ C/Lab1.6/lab1.6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int h, m;
+ 
+    scanf_s("%d %d", &h, &m);
+
+    // Переводим часы в угол
+    float hourAngle = (h % 12) * 30 + m / 2.0;
+    // Переводим минуты в угол
+    float minuteAngle = m * 6;
+
+    // Вычисляем разницу между углами
+    float angleDiff = hourAngle - minuteAngle;
+    if (angleDiff < 0)
+    {
+        angleDiff = -angleDiff;
+    }
+
+    // Вычисляем наименьшее время в минутах
+    int timeInMinutes = angleDiff / 5 * 60;
+    printf("%d ", timeInMinutes);
+
+    return 0;
+}
