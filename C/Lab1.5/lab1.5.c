4f15f29cfd84be07d7ac2480dac051760149906e
3uPaeJLb <131700005+3uPaeJLb@users.noreply.github.com>
2023-09-19 21:56:39 +0700

    Create lab1.5.c

---
+++ C/Lab1.5/lab1.5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int h, m;
+
+    scanf_s("%d", &h);
+    scanf_s("%d", &m);
+
+    printf("%d", ((h % 12 * 5 + 15 * (h % 12 * 5 + 15 - m) / abs((h % 12 * 5 + 15 - m))) + 60 - m) % 60);
+
+	return 0;
+
+}
