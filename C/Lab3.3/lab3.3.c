360249a2b4ca3daf512c9204dbb8219e90526ff3
3uPaeJLb <131700005+3uPaeJLb@users.noreply.github.com>
2023-09-19 22:08:14 +0700

    Create lab3.3.c

---
+++ C/Lab3.3/lab3.3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+	int number = 0;
+	int back_number[5];
+	int shift = 10;
+
+	scanf_s("%d", &number);
+
+	for (int i = 0; i < 5; i++)
+	{
+		back_number[i] = number % shift;
+		number /= 10;
+	}
+
+	for (int i = 0; i < 5; i++)
+		printf("%d", back_number[i]);
+
+	return 0;
+}
