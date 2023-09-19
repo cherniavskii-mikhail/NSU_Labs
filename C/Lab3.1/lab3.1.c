62a8e7f07cebb5d9bab00e2d30d03a199162a1f8
3uPaeJLb <131700005+3uPaeJLb@users.noreply.github.com>
2023-09-19 22:06:23 +0700

    Create lab3.1.c

---
+++ C/Lab3.1/lab3.1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int start_number = 0;
+	int rez = 0;
+
+	scanf_s("%d", &start_number);
+
+	rez += pow(start_number, 2);
+
+	for (int i = start_number - 1; i > 0; i--)
+		rez -= pow(i, 2);
+	
+
+	printf("Rezult: %d", rez);
+	return 0;
+}
