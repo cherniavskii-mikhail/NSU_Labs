41cb678ed77612fb41de2238626e3f040e036f90
3uPaeJLb <131700005+3uPaeJLb@users.noreply.github.com>
2023-09-19 21:50:12 +0700

    Create lab1.1.c

---
+++ C/Lab1.1/lab1.1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    int a;
+    int a1, a2, a3;
+    int a4, a5, a6;
+
+    scanf_s("%d", &a);
+
+    a1 = a * a;     // a^2
+    a2 = a1 * a1; // a**4
+    a3 = a2 * a2; // a**8
+    a4 = a3 * a3; // a**16
+    a5 = a4 * a3; // a**24
+    a6 = a5 * a2; // a**28
+
+    printf("%d", a6);
+
+    return 0;
+}
