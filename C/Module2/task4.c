fb914f39465b29ae0958a88044757bc13195be0a
3uPaeJLb <131700005+3uPaeJLb@users.noreply.github.com>
2023-09-22 14:12:21 +0700

    Create task4.c

---
+++ C/Module2/task4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+
+int numbers_lengh(int* numbers, int n)
+{
+    int len = 0, max_len = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (numbers[i] % 2 != 0)
+            len++;
+        else
+        {
+            if (max_len < len)
+                max_len = len;
+            len = 0;
+        }
+    }
+
+    if (max_len < len)
+        max_len = len;
+
+    return max_len;
+}
+
+int main()
+{
+    int* numbers = 0, n = 0, len = 0;
+
+    scanf_s("%d", &n);
+    numbers = (int*)malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++)
+        scanf_s("%d", numbers + i);
+
+    len = numbers_lengh(numbers, n);
+
+    printf("%d", len);
+    return 0;
+}
