f913f038ca4f10093117726b8848a888099551c4
3uPaeJLb <131700005+3uPaeJLb@users.noreply.github.com>
2023-10-03 13:22:21 +0700

    Create task1.c

---
+++ C/Module3/task1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+    char str[80];
+    int shift_numb = 0, str_lh = 0;
+
+    gets(str);
+
+    str_lh = strlen(str);
+
+    for (int i = 0; i < str_lh; i++)
+    {
+        if (str[i] == ' ')
+            shift_numb++;
+    }
+
+    printf("%d", shift_numb);
+    re
