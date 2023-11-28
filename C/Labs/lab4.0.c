af355170450803e637f652f052e14b3914379f33
3uPaeJLb <131700005+3uPaeJLb@users.noreply.github.com>
2023-11-28 09:51:04 +0700

    Update lab4.0.c

--- C/Labs/lab4.0.c
+++ C/Labs/lab4.0.c
@@ -4,32 +4,42 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+
 #define MAX_LENGTH 1004
 
-void PushStack(stack* operator_stack, int symbol) // эта функция пихает операторы в стэк операторо
+struct stack
 {
-    operator_stack->mas[operator_stack->size] = symbol;
-    operator_stack->size++;
+    char value;
+    struct sist* next;
+};
+
+/* эта функция пихает операторы в стэк операторов*/
+void PushStack(struct stack* operator_stack, int symbol)
+{
+    operator_stack->value = symbol;
+    operator_stack->next++;
 }
 
-int PopStack(stack* operator_stack) // эта функция вытаскивает оператор из стека операторов и уменьшает размер стэка
+/* эта функция вытаскивает оператор из стека операторов и уменьшает размер стэка*/
+int pop_stack(struct stack* operator_stack)
 {
-    if (operator_stack->size == 0)
+    if (operator_stack->value == 0)
     {
         printf("syntax error!");
-        exit(0);
+        return 0;
     }
-    operator_stack->size--;
-    return operator_stack->mas[operator_stack->size];
+    operator_stack->value--;
+    return operator_stack->value;
 }
 
-int PeekStack(stack* operator_stack) // эта функция просто берёт оператор, но не уменьшает размер стэка
+/* эта функция просто берёт оператор, но не уменьшает размер стэка*/
+int pop_stack(struct stack* operator_stack) 
 {
-    if (operator_stack->size <= 0) {
+    if (operator_stack->value <= 0) {
         printf("syntax error");
-        exit(0);
+        return 0;
     }
-    return operator_stack->mas[operator_stack->size - 1];
+    return operator_stack->value;
 }
 
 int priority(char c)
@@ -44,7 +54,17 @@ int priority(char c)
 }
 int isOperator(char symbol)
 {
-    return symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/' || symbol == '(' || symbol == ')';
+    int check = 0;
+
+    if (symbol == '+' || symbol == '-')
+        check = 1;
+    else if (symbol == '*' || symbol == '/')
+        check = 1;
+    else if (symbol == '(' || symbol == ')')
+        check = 1;
+        
+    
+    return check;
 }
 
 int proverka_simvolov(char symbol)
@@ -52,63 +72,63 @@ int proverka_simvolov(char symbol)
     return isOperator(symbol) || isdigit(symbol) || symbol == '\n';
 }
 
-void postfix(char* stroka, int size, char* postf)
+void postfix(char* str, int size, char* postf)
 {
     int postfix_size = 0;
-    stack operator_stack;
-    operator_stack.size = 0;
-    for (int i = 0; i < size; ++i) // идём с начала выражения и проверяем число это или оператор
+    struct stack operator_stack;
+    operator_stack.value = 0;
+    for (int i = 0; i < size; ++i) 
     {
-        if (isdigit(stroka[i])) // если попалось число
+        if (isdigit(str[i])) 
         {
-            while (!isOperator(stroka[i])) // пока идёт число, записываем его в массив postf
+            while (!isOperator(str[i])) 
             {
-                postf[postfix_size] = stroka[i];
+                postf = str[i];
                 postfix_size++;
                 i++;
-                if (i == size) break; // если получилось, что длина числа равна длине строки, то заканчиваем
+                if (i == size) break;
             }
-            postf[postfix_size] = ' ';
+            postf = ' ';
             postfix_size++;
             i--;
         }
-        if (isOperator(stroka[i])) // если попался оператор
+        if (isOperator(str[i]))
         {
-            if (stroka[i] == '(') // если попавшийся символ это открывающая скобка
+            if (str[i] == '(') 
             {
-                PushStack(&operator_stack, (int)stroka[i]); // кидаем скобку в стек операторов
+                PushStack(&operator_stack, (int)str[i]); 
             }
-            else if (stroka[i] == ')') // если попавшийся символ закрывающая скобка
+            else if (str[i] == ')') 
             {
-                if (i == 0 || stroka[i - 1] == '(') // тут мы проверяем, на первом месте не стоит закрывающая скобка или или предыдущий символ не является открывающей скобкой
+                if (i == 0 || str[i - 1] == '(') 
                 {
                     printf("syntax error!");
                     exit(0);
                 }
-                char s = (int)PopStack(&operator_stack); // вытащили из стека оператор
-                while (s != '(')  // теперь из стека операторов выгружаем все операторы до открывающей скобки в постфиксную запись
+                char s = (int)pop_stack(&operator_stack); 
+                while (s != '(')  
                 {
                     postf[postfix_size] = s;
                     postf[postfix_size + 1] = ' ';
                     postfix_size += 2;
-                    s = (int)PopStack(&operator_stack);
+                    s = (int)pop_stack(&operator_stack);
                 }
             }
             else
             {
-                if (operator_stack.size > 0 && priority(stroka[i]) <= priority((char)PeekStack(&operator_stack)))
+                if (operator_stack.value > 0 && priority(str[i]) <= priority((char)pop_stack(&operator_stack)))
                 {
-                    postf[postfix_size] = (char)PopStack(&operator_stack);
+                    postf[postfix_size] = (char)pop_stack(&operator_stack);
                     postf[postfix_size + 1] = ' ';
                     postfix_size += 2;
                 }
-                PushStack(&operator_stack, (int)stroka[i]); // тут в стэк операторов грузятся осставшиеся операторы
+                PushStack(&operator_stack, (int)str[i]); 
             }
         }
     }
-    while (operator_stack.size > 0) 
+    while (operator_stack.value > 0)
     {
-        postf[postfix_size] = (char)PopStack(&operator_stack);
+        postf[postfix_size] = (char)pop_stack(&operator_stack);
         postf[postfix_size + 1] = ' ';
         postfix_size += 2;
     }
@@ -118,8 +138,8 @@ void postfix(char* stroka, int size, char* postf)
 int calculate(char* postf)
 {
     int resultat = 0;
-    stack resultat_stack;
-    resultat_stack.size = 0;
+    struct stack resultat_stack;
+    resultat_stack.value = 0;
     /*size_t*/int postf_size = strlen(postf);
     for (int i = 0; i < postf_size; ++i)
     {
@@ -137,9 +157,10 @@ int calculate(char* postf)
             PushStack(&resultat_stack, x);
             i--;
         }
+
         else if (isOperator(postf[i])) {
-            int a = PopStack(&resultat_stack);
-            int b = PopStack(&resultat_stack);
+            int a = pop_stack(&resultat_stack);
+            int b = pop_stack(&resultat_stack);
             if (postf[i] == '+') {
                 resultat = b + a;
             }
@@ -159,33 +180,33 @@ int calculate(char* postf)
             PushStack(&resultat_stack, resultat);
         }
     }
-    return PeekStack(&resultat_stack);
+    return peek_stack(&resultat_stack);
 }
 
 int main(void)
 {
-    char stroka[MAX_LENGTH];  // тут лежит выражение , которое мы должны посчитать
-    while (!fgets(stroka, MAX_LENGTH, stdin))
+    char str[MAX_LENGTH];
+    while (!fgets(str, MAX_LENGTH, stdin))
     {
-        printf("syntax error!");      // ввод строки которую нужно будет посчитать
+        printf("syntax error!");    
         exit(0);
     }
-     
-    size_t ammount = strlen(stroka); // длина это строки которую мы считаем
+
+    int ammount = strlen(str);
     unsigned int i = 0;
 
-    while (i < (ammount - 1))// тут как-то странно, вроде не надо отнимать, т.к. счютчик идёт с нуля
+    while (i < (ammount - 1))
     {
-        if (!proverka_simvolov(stroka[i]))
-        { 
-            printf("syntax error");   // проверяем символы, чтобы они не были какими-нибудь левыми
+        if (!proverka_simvolov(str[i]))
+        {
+            printf("syntax error");  
             return 0;
         }
         i++;
     }
-    stroka[i] = '\0'; 
-    char postf[MAX_LENGTH * 5]; // в этот массив будет записана постфиксная запись
-    postfix(stroka, i, postf);
+    str[i] = '\0';
+    char postf;
+    postfix(str, i, postf);
     printf("%d", calculate(postf));
     return 0;
 }
